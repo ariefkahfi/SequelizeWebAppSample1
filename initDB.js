3211b775ece751549e73afb0c9f10cd0d3a18dba
ariefkahfi <ariefkahfi@gmail.com>
2018-03-19 22:43:03 +0700

    commit for join query on driver and bus

--- initDB.js
+++ initDB.js
@@ -1,6 +1,6 @@
 const DatabaseModel = require("./models/db-model");
 const dbModel = new DatabaseModel();
 
-dbModel.syncAllTables(true)
-.then(val=> console.log(val))
-.catch(err=> console.error(err))
+// dbModel.syncAllTables(true)
+// .then(val=> console.log(val))
+// .catch(err=> console.error(err))
